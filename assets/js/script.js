9f37a7c2a86bec82b48321bd05d0e67b9bc58089
makoto-yamaguchi <makoto-yamaguchi.s@hisoftec.com>
2023-10-13 18:22:16 +0900

    script修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register("/sw.js").then((reg) => {
-            console.log("Service worker registered.", reg);
-        });
-    });
-}
-  
-// プッシュ通知の設定
-window.OneSignal = window.OneSignal || [];
-OneSignal.push(function () {
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+OneSignalDeferred.push(function(OneSignal) {
     OneSignal.init({
         appId: "21e490aa-9fa9-4431-8ad9-de9f1f5863a3",
+        notifyButton: {
+            enable: true,
+        },
     });
 });
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('OneSignalSDKWorker.js')
+                .then(function() { console.log("Service Worker Registered"); })
+                .catch(function() { console.log("Service Worker Not Registered"); });
+    });
+}
